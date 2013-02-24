a09f9f6fef04ca8d78e1b3f0dadb4d3a8e4760a5
Dan Deng <dan.deng@duke.edu>
2013-02-23 21:53:39 -0500

    first commit

---
+++ table.php
@@ -0,0 +1,114 @@
+<html lang = "en">
+<head>
+        <link href="bootstrap.css" rel="stylesheet">
+
+        <script src="http://code.jquery.com/jquery-latest.js">     </script>
+        <script src="bootstrap.js"></script>
+        <style type="text/css">
+          table.db-table     { border-right:1px solid #ccc; border-bottom:1px solid #ccc; }
+table.db-table th  { background:#eee; padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+table.db-table td  { padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+         
+         p {color:red;}
+
+         g {color:red;}
+
+         b {color: green;}
+
+        </style>
+</head>
+<body>
+   <div class = "container">
+   <div class = "toprow">
+     </div>
+  <div class = "masthead">
+                        <ul class = "nav nav-pills pull-right">
+                            <li>
+                                <a class="btn btn-link" href="MainPage.html">Main</a>
+                            </li>
+                            
+                             <li class="dropdown">
+                                <a class="dropdown-toggle"
+                                   data-toggle="dropdown"
+                                   href="#">
+                                    Account
+                                    <b class="caret"></b>
+                                  </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="btn btn-link" href = "table.php#" id = "account">Items</a></li>
+                                    <li><a class="btn btn-link" href = "homepage.html#">Log out</a></li>
+                                  <!-- links -->
+                                </ul>
+                              </li>
+                      
+                         
+                        </ul>
+                </div>
+  <div class = "row"> </div>
+    <hr>
+
+<div align = "center">
+  <h2> My Items </h2>
+</div>
+
+<div align = "center">
+If the price of any of the items you are tracking falls, the red <g> No </g> will become a green <b> Yes</b>.
+Then you are good to go!
+<div>
+  <div>
+    &nbsp
+  </div>
+
+<div align="center">
+<?php
+
+$host = "mysql10.000webhost.com";
+$user = "a7487523_root";
+$db_name = "a7487523_sticker";
+$password= "password1";
+
+$con = mysql_connect($host, $user, $password);
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db($db_name, $con);
+
+$result = mysql_query("SELECT * FROM items");
+
+echo "<table border='2' class = 'db-table'>
+<tr>
+<th>Item Name</th>
+<th>Store Name</th>
+<th>Item Price ($)</th>
+<th>Desired Price ($)</th>
+<th> URL </th>
+<th>Buy?</th>
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td> " . $row['itemName'] . " </td>";
+  echo "<td> " . $row['storeName'] . " </td>";
+  echo "<td> " . $row['itemPrice'] . " </td>";
+  echo "<td> " . $row['desiredPrice'] . " </td>";
+  $url = $row['link'];
+  echo "<td> <a href = $url> Link </a> </td>";
+  echo "<td> <p> No </p> </td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+?>
+<div/>
+
+
+</body>
+
+
+    <div class="navbar navbar-fixed-bottom" align = 'left'>
+      <a>  &copy; Dan Deng</a>
+    </div>
