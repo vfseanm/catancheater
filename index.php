b5cd6db29a56bd720960d6ecb3bf10c1eaa7a023
Dan Deng <dan.deng@duke.edu>
2013-03-13 01:41:02 -0400

    demo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 
-<?php phpinfo(); 
+Echo "Hello World!"
 
 ?>
 
