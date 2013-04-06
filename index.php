213c269241388ff4d161f05b92ed19a90d18e488
vfseanm@gmail.com <vfseanm@gmail.com>
2013-04-06 15:35:13 -0400

    first push

---
+++ index.php
@@ -0,0 +1,316 @@
+
+<!DOCTYPE html> 
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<title>Catan Cheater</title> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head> 
+
+	
+<body> 
+
+<!-- Start of first page: #one -->
+<div data-role="page" id="one">
+
+	<div data-role="header">
+		<h1>Catan Cheater</h1>
+	</div><!-- /header -->
+		<p><a href="#popup" data-role="button" data-rel="dialog" data-transition="pop">Show page "popup" (as a dialog)</a></p>
+
+	<div data-role="content" >	
+		<h2>Board Setup</h2>
+
+<form action="setup_board.php" method="get" class="ui-body ui-body-a ui-corner-all" data-ajax="false">
+		<fieldset>
+		
+		
+		<h3>A1:</h3>	
+		<label for="select-choice-a1" class="select">Resource:</label>
+			<select name="select-choice-a1" id="select-choice-a1">
+   				<option value="wood">Wood</option>
+   				<option value="sheep">Sheep</option>
+   				<option value="grains">Grain</option>
+   				<option value="ore">Ore</option>
+      			<option value="brick">Brick</option>
+			</select>
+		<label for="slider-a1">Number</label>
+			<input type="range" name="slider-a1" id="slider-a1" value="7" min="0" max="12" />
+
+			<h3>A2:</h3>	
+		<label for="select-choice-a2" class="select">Resource:</label>
+			<select name="select-choice-a2" id="select-choice-a2">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-a3">Number</label>
+			<input type="range" name="slider-a3" id="slider-a3" value="7" min="0" max="12" />
+
+			<h3>A3:</h3>	
+		<label for="select-choice-a3" class="select">Resource:</label>
+			<select name="select-choice-a3" id="select-choice-a3">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-a3">Number</label>
+			<input type="range" name="slider-1" id="slider-a3" value="7" min="0" max="12" />
+
+			<h3>A4:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A5:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A6:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A7:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A8:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A9:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A10:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A11:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A12:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A13:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A14:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A15:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A16:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A17:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A4:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+			<h3>A4:</h3>	
+		<label for="select-choice-0" class="select">Resource:</label>
+			<select name="select-choice-0" id="select-choice-0">
+   				<option value="standard">Wood</option>
+   				<option value="rush">Sheep</option>
+   				<option value="express">Grain</option>
+   				<option value="overnight">Ore</option>
+      			<option value="overnight">Brick</option>
+			</select>
+		<label for="slider-1">Number</label>
+			<input type="range" name="slider-1" id="slider-1" value="7" min="0" max="12" />
+
+		<button type="submit" data-theme="b" name="submit" value="submit-value">Submit</button>
+	</fieldset>
+	</form>
+
+
+	</div><!-- /content -->
+	
+	<div data-role="footer" data-theme="d">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div><!-- /page one -->
+
+
+<!-- Start of second page: #two -->
+<div data-role="page" id="two" data-theme="a">
+
+	<div data-role="header">
+		<h1>Two</h1>
+	</div><!-- /header -->
+
+	<div data-role="content" data-theme="a">	
+		<h2>Two</h2>
+		<p>I have an id of "two" on my page container. I'm the second page container in this multi-page template.</p>	
+		<p>Notice that the theme is different for this page because we've added a few <code>data-theme</code> swatch assigments here to show off how flexible it is. You can add any content or widget to these pages, but we're keeping these simple.</p>	
+		<p><a href="#one" data-direction="reverse" data-role="button" data-theme="b">Back to page "one"</a></p>	
+		
+	</div><!-- /content -->
+	
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div><!-- /page two -->
+
+
+<!-- Start of third page: #popup -->
+<div data-role="page" id="popup">
+
+	<div data-role="header" data-theme="e">
+		<h1>Board</h1>
+	</div><!-- /header -->
+
+	<div data-role="content" data-theme="d">	
+		<h2>Popup</h2>
+		<p>This is the Board.</p>	
+		<p>This is the Board.</p>	
+			<p>This is the Board.</p>	
+			<p>This is the Board.</p>	
+		<p><a href="#one" data-rel="back" data-role="button" data-inline="true" data-icon="back">Back to page "one"</a></p>	
+	</div><!-- /content -->
+	
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div><!-- /page popup -->
+
+</body>
+</html>
