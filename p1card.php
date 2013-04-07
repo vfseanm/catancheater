dde1ae4fe52a2ab3d2ddc73d9b57cd08a040f1c4
Dan Deng <dan.deng@duke.edu>
2013-04-07 10:33:44 -0400

    demo

--- p1card.php
+++ p1card.php
@@ -4,47 +4,83 @@
 
 
 ?>
+<html>
 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<title>Multi-page template</title> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.js"></script>
+</head> 
+<body>
 <div data-role="page" id="p1card">
 
 	<div data-role="header">
-		<h1>Player One's cards</h1>
+		<h1>Player One</h1>
 	</div><!-- /header -->
 
 	<div data-role="content" >	
 		<h2>I have...</h2>
+		<img src = "wood.png">
 		<?php
-
-
-
-
+		$con=mysqli_connect("localhost","root","root","catan");
+		$result = mysqli_query($con,"SELECT * FROM players WHERE ID =1");
+		while($row = mysqli_fetch_array($result))
+  			{
+  		    echo "Wood: ";
+  		    echo $row['Wood'];
+  		    echo "<br>";
+  			}
+  		mysqli_close($con);	
+		?>
+		<img src = "sheep.png">
+		<?php
+		$con=mysqli_connect("localhost","root","root","catan");
+		$result = mysqli_query($con,"SELECT * FROM players WHERE ID =1");
+		while($row = mysqli_fetch_array($result))
+  			{
+  		    echo "Sheep: ";
+  		    echo $row['Sheep'];
+  		    echo "<br>";
+  			}
+  		mysqli_close($con);	
+		?>
+		<img src = "grain.png">
+		<?php
+		$con=mysqli_connect("localhost","root","root","catan");
+		$result = mysqli_query($con,"SELECT * FROM players WHERE ID =1");
+		while($row = mysqli_fetch_array($result))
+  			{
+  		    echo "Grain: ";
+  		    echo $row['Grain'];
+  		    echo "<br>";
+  			}
+  		mysqli_close($con);	
+		?>
+		<img src = "bricks.png">
+		<?php
+		$con=mysqli_connect("localhost","root","root","catan");
+		$result = mysqli_query($con,"SELECT * FROM players WHERE ID =1");
+		while($row = mysqli_fetch_array($result))
+  			{
+  		    echo "Bricks: ";
+  		    echo $row['Bricks'];
+  		    echo "<br>";
+  			}
+  		mysqli_close($con);	
 		?>
 
-		
 	</div>
 
-	<form action="create.php" method="get">
-		<button type="submit" data-theme="b" onclick="goToSetUp()" name="submit" value="submit-value">Set up game</button>
-	</form>
 
-	<script>
-
-
-	function goToSetUp(){
-	window.location = "/#main"
-	}
-
-	</script>
-
-	</div><!-- /content -->
-	
 	<div data-role="footer" data-theme="d">
 		<h4>Page Footer</h4>
 	</div><!-- /footer -->
 </div><!-- /page one -->
 
+</body>
 
-<!-- Start of second page: #main -->
-<div data-role="page" id="main" data-theme="b">
-<?php
- include "setup-board-display.php";
+</html>
+
