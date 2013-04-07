da350bd3ab763a370ad8b936a8085f9a79b2ef65
CarlosERS <Japan540@aol.com>
2013-04-06 21:22:57 -0400

    bfas

---
+++ Port.php
@@ -0,0 +1,9 @@
+<?php
+
+class Port{
+	public $ratio;
+	public $type;
+
+}
+
+?>
---
+++ Settlement.php
@@ -0,0 +1,12 @@
+<?php
+
+class Settlement{
+	public $playerName;
+	public $settlementExists;
+	public $settlementCoordinteX;
+	public $settlementCoordinteY;
+	public $port;
+
+}
+
+?>
