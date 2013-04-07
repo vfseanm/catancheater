ecb1532193c5fe2dc5f51eaaddeedb9398230127
vfseanm@gmail.com <vfseanm@gmail.com>
2013-04-07 01:22:36 -0400

    fdfd

--- build_form.php
+++ build_form.php
@@ -3,42 +3,41 @@
 		<fieldset>
 		
 
-		<label for="1st-select" class="select">1st Bordering Hex</label>
-			<select id="1st_select">
-   				<option value="1">H1</option>
-   				<option value="2">H2</option>
-   				<option value="3">H3</option>
-   				<option value="4">H4</option>
-   				<option value="5">H5</option>
-   				<option value="6">H6</option>
-   				<option value="7">H7</option>
-   				<option value="8">H8</option>
+		<label for="first" class="select">1st Bordering Hex</label>
+			<select id="first" name="first">
+   				<option value="h1">H1</option>
+   				<option value="h2">H2</option>
+   				<option value="h3">H3</option>
+   				<option value="h4">H4</option>
+   				<option value="h5">H5</option>
+   				<option value="h6">H6</option>
+   				<option value="h7">H7</option>
+   				<option value="h8">H8</option>
 			</select>
-			<label for="2nd-select" class="select">2nd Bordering Hex</label>
-			<select  id="2nd-select">
-   				<option value="1">H1</option>
-   				<option value="2">H2</option>
-   				<option value="3">H3</option>
-   				<option value="4">H4</option>
-   				<option value="5">H5</option>
-   				<option value="6">H6</option>
-   				<option value="7">H7</option>
-   				<option value="8">H8</option>
-
+			<label for="second" class="select">2nd Bordering Hex</label>
+			<select  id="second" name="second">
+   				<option value="h1">H1</option>
+   				<option value="h2">H2</option>
+   				<option value="h3">H3</option>
+   				<option value="h4">H4</option>
+   				<option value="h5">H5</option>
+   				<option value="h6">H6</option>
+   				<option value="h7">H7</option>
+   				<option value="h8">H8</option>
 			</select>
-			<label for="3rd-select" class="select">3rd Bordering Hex</label>
-			<select id="3rd-select">
-   				<option value="1">H1</option>
-   				<option value="2">H2</option>
-   				<option value="3">H3</option>
-   				<option value="4">H4</option>
-   				<option value="5">H5</option>
-   				<option value="6">H6</option>
-   				<option value="7">H7</option>
-   				<option value="8">H8</option>
+			<label for="third" class="select">3rd Bordering Hex</label>
+			<select id="third" name="third">
+   				<option value="h1">H1</option>
+   				<option value="h2">H2</option>
+   				<option value="h3">H3</option>
+   				<option value="h4">H4</option>
+   				<option value="h5">H5</option>
+   				<option value="h6">H6</option>
+   				<option value="h7">H7</option>
+   				<option value="h8">H8</option>
 			</select>
          <br />
-         <input type="hidden" name="playerID" value="1">
+         <input 
 			<button type="submit" data-theme="b" name="submit" value="submit-value">Submit</button>
 	</fieldset>
 	</form>
