25239441e261819ec81b45249728f262d1446f1f
CarlosERS <Japan540@aol.com>
2013-04-07 09:37:24 -0400

    asd

--- setup_board1.php
+++ setup_board1.php
@@ -43,71 +43,71 @@ function resourceList(){
 	public $vertecies;
 	$numberMap = array(2=>1, 3=>2, 4=>3, 5=>4, 6=>5, 8=>5, 9=>4, 10=>3, 11=>2, 12=>1);
 
-	$v0 = ($numberMaps[resourceArray[0]->number], 0);
-	$v1 = ($numberMaps[resourceArray[0]->number], 1);
-	$v2 = ($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[1]->number], 2);
-	$v3 = ($numberMaps[resourceArray[1]->number], 3);
-	$v4 = ($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[1]->number], 4);
-	$v5 = ($numberMaps[resourceArray[2]->number], 5);
-	$v6 = ($numberMaps[resourceArray[2]->number], 6);
+	$vertArray[0] = array($numberMaps[resourceArray[0]->number], 0);
+	$vertArray[1] = array($numberMaps[resourceArray[0]->number], 1);
+	$vertArray[2] = array($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[1]->number], 2);
+	$vertArray[3] = array($numberMaps[resourceArray[1]->number], 3);
+	$vertArray[4] = array($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[1]->number], 4);
+	$vertArray[5] = array($numberMaps[resourceArray[2]->number], 5);
+	$vertArray[6] = array($numberMaps[resourceArray[2]->number], 6);
 
-	$v7 = ($numberMaps[resourceArray[3]->number], 7);
-	$v8 = ($numberMaps[resourceArray[3]->number] + $numberMaps[resourceArray[0]->number], 8);
-	$v9 = ($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[3]->number]+ $numberMaps[resourceArray[4]->number], 9);
-	$v10 = ($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[1]->number]+ $numberMaps[resourceArray[4]->number], 10);
-	$v11 = ($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[5]->number], 11;
-	$v12 = ($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[2]->number]+ $numberMaps[resourceArray[5]->number], 12);
-	$v13 = ($numberMaps[resourceArray[2]->number] + $numberMaps[resourceArray[5]->number]+ $numberMaps[resourceArray[6]->number], 13);
-	$v14 = ($numberMaps[resourceArray[2]->number] + $numberMaps[resourceArray[6]->number], 14);
-	$v15 = ($numberMaps[resourceArray[6]->number], 15);
+	$vertArray[7] = ($numberMaps[resourceArray[3]->number], 7);
+	$vertArray[8] = ($numberMaps[resourceArray[3]->number] + $numberMaps[resourceArray[0]->number], 8);
+	$vertArray[9] = ($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[3]->number]+ $numberMaps[resourceArray[4]->number], 9);
+	$vertArray[10] = ($numberMaps[resourceArray[0]->number] + $numberMaps[resourceArray[1]->number]+ $numberMaps[resourceArray[4]->number], 10);
+	$vertArray[11] = ($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[5]->number], 11;
+	$vertArray[12] = ($numberMaps[resourceArray[1]->number] + $numberMaps[resourceArray[2]->number]+ $numberMaps[resourceArray[5]->number], 12);
+	$vertArray[13] = ($numberMaps[resourceArray[2]->number] + $numberMaps[resourceArray[5]->number]+ $numberMaps[resourceArray[6]->number], 13);
+	$vertArray[14] = ($numberMaps[resourceArray[2]->number] + $numberMaps[resourceArray[6]->number], 14);
+	$vertArray[15] = ($numberMaps[resourceArray[6]->number], 15);
 
-	$v16 = ($numberMaps[resourceArray[7]->number], 16);
-	$v17 = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[3]->number], 17);
-	$v18 = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[3]->number]+ $numberMaps[resourceArray[8]->number], 18);
-	$v19 = ($numberMaps[resourceArray[3]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[8]->number], 19);
-	$v20 = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[9]->number], 20);
-	$v21 = ($numberMaps[resourceArray[4]->number] + $numberMaps[resourceArray[5]->number]+ $numberMaps[resourceArray[9]->number], 21);
-	$v22 = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[5]->number] + $numberMaps[resourceArray[10]->number], 22);
-	$v23 = ($numberMaps[resourceArray[5]->number] + $numberMaps[resourceArray[6]->number]+ $numberMaps[resourceArray[10]->number], 23);
-	$v24 = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[6]->number]+ $numberMaps[resourceArray[11]->number], 24);
-	$v25 = ($numberMaps[resourceArray[11]->number] + $numberMaps[resourceArray[6]->number], 25);
-	$v26 = ($numberMaps[resourceArray[11]->number], 26);
+	$vertArray[16] = ($numberMaps[resourceArray[7]->number], 16);
+	$vertArray[17] = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[3]->number], 17);
+	$vertArray[18] = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[3]->number]+ $numberMaps[resourceArray[8]->number], 18);
+	$vertArray[19] = ($numberMaps[resourceArray[3]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[8]->number], 19);
+	$vertArray[20] = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[4]->number]+ $numberMaps[resourceArray[9]->number], 20);
+	$vertArray[21] = ($numberMaps[resourceArray[4]->number] + $numberMaps[resourceArray[5]->number]+ $numberMaps[resourceArray[9]->number], 21);
+	$vertArray[22] = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[5]->number] + $numberMaps[resourceArray[10]->number], 22);
+	$vertArray[23] = ($numberMaps[resourceArray[5]->number] + $numberMaps[resourceArray[6]->number]+ $numberMaps[resourceArray[10]->number], 23);
+	$vertArray[24] = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[6]->number]+ $numberMaps[resourceArray[11]->number], 24);
+	$vertArray[25] = ($numberMaps[resourceArray[11]->number] + $numberMaps[resourceArray[6]->number], 25);
+	$vertArray[26] = ($numberMaps[resourceArray[11]->number], 26);
 
-	$v27 = ($numberMaps[resourceArray[7]->number], 27);
-	$v28 = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[12]->number], 28);
-	$v29 = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[8]->number]+ $numberMaps[resourceArray[12]->number], 29);
-	$v30 = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[12]->number]+ $numberMaps[resourceArray[13]->number], 30);
-	$v31 = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[9]->number]+ $numberMaps[resourceArray[13]->number], 31);
-	$v32 = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[13]->number]+ $numberMaps[resourceArray[14]->number], 32);
-	$v33 = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[14]->number], 33);
-	$v34 = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[14]->number]+ $numberMaps[resourceArray[15]->number], 34);
-	$v35 = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[11]->number]+ $numberMaps[resourceArray[15]->number], 35);
-	$v36 = ($numberMaps[resourceArray[11]->number] + $numberMaps[resourceArray[15]->number];
-	$v37 = ($numberMaps[resourceArray[11]->number], 36);
+	$vertArray[27] = ($numberMaps[resourceArray[7]->number], 27);
+	$vertArray[28] = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[12]->number], 28);
+	$vertArray[29] = ($numberMaps[resourceArray[7]->number] + $numberMaps[resourceArray[8]->number]+ $numberMaps[resourceArray[12]->number], 29);
+	$vertArray[30] = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[12]->number]+ $numberMaps[resourceArray[13]->number], 30);
+	$vertArray[31] = ($numberMaps[resourceArray[8]->number] + $numberMaps[resourceArray[9]->number]+ $numberMaps[resourceArray[13]->number], 31);
+	$vertArray[32] = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[13]->number]+ $numberMaps[resourceArray[14]->number], 32);
+	$vertArray[33] = ($numberMaps[resourceArray[9]->number] + $numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[14]->number], 33);
+	$vertArray[34] = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[14]->number]+ $numberMaps[resourceArray[15]->number], 34);
+	$vertArray[35] = ($numberMaps[resourceArray[10]->number] + $numberMaps[resourceArray[11]->number]+ $numberMaps[resourceArray[15]->number], 35);
+	$vertArray[36] = ($numberMaps[resourceArray[11]->number] + $numberMaps[resourceArray[15]->number];
+	$vertArray[37] = ($numberMaps[resourceArray[11]->number], 36);
 
-	$v38 = ($numberMaps[resourceArray[12]->number], 38);
-	$v39 = ($numberMaps[resourceArray[12]->number] + $numberMaps[resourceArray[16]->number], 39);
-	$v40 = ($numberMaps[resourceArray[12]->number] + $numberMaps[resourceArray[13]->number]+ $numberMaps[resourceArray[16]->number], 40);
-	$v41 = ($numberMaps[resourceArray[13]->number] + $numberMaps[resourceArray[16]->number]+ $numberMaps[resourceArray[17]->number], 41);
-	$v42 = ($numberMaps[resourceArray[13]->number] + $numberMaps[resourceArray[14]->number]+ $numberMaps[resourceArray[17]->number], 42);
-	$v43 = ($numberMaps[resourceArray[14]->number] + $numberMaps[resourceArray[17]->number]+ $numberMaps[resourceArray[18]->number], 43);
-	$v44 = ($numberMaps[resourceArray[14]->number] + $numberMaps[resourceArray[18]->number]+ $numberMaps[resourceArray[15]->number], 44);
-	$v45 = ($numberMaps[resourceArray[15]->number] + $numberMaps[resourceArray[18]->number], 45);
-	$v46 = ($numberMaps[resourceArray[15]->number], 46);
+	$vertArray[38] = ($numberMaps[resourceArray[12]->number], 38);
+	$vertArray[39] = ($numberMaps[resourceArray[12]->number] + $numberMaps[resourceArray[16]->number], 39);
+	$vertArray[40] = ($numberMaps[resourceArray[12]->number] + $numberMaps[resourceArray[13]->number]+ $numberMaps[resourceArray[16]->number], 40);
+	$vertArray[41] = ($numberMaps[resourceArray[13]->number] + $numberMaps[resourceArray[16]->number]+ $numberMaps[resourceArray[17]->number], 41);
+	$vertArray[42] = ($numberMaps[resourceArray[13]->number] + $numberMaps[resourceArray[14]->number]+ $numberMaps[resourceArray[17]->number], 42);
+	$vertArray[43] = ($numberMaps[resourceArray[14]->number] + $numberMaps[resourceArray[17]->number]+ $numberMaps[resourceArray[18]->number], 43);
+	$vertArray[44] = ($numberMaps[resourceArray[14]->number] + $numberMaps[resourceArray[18]->number]+ $numberMaps[resourceArray[15]->number], 44);
+	$vertArray[45] = ($numberMaps[resourceArray[15]->number] + $numberMaps[resourceArray[18]->number], 45);
+	$vertArray[46] = ($numberMaps[resourceArray[15]->number], 46);
 
-	$v47 = ($numberMaps[resourceArray[16]->number], 47);
-	$v48 = ($numberMaps[resourceArray[16]->number], 48);
-	$v49 = ($numberMaps[resourceArray[16]->number] + $numberMaps[resourceArray[17]->number], 49);
-	$v50 = ($numberMaps[resourceArray[17]->number], 50);
-	$v51 = ($numberMaps[resourceArray[17]->number] + $numberMaps[resourceArray[18]->number], 51);
-	$v52 = ($numberMaps[resourceArray[18]->number], 52);
-	$v53 = ($numberMaps[resourceArray[18]->number], 53);
+	$vertArray[47] = ($numberMaps[resourceArray[16]->number], 47);
+	$vertArray[48] = ($numberMaps[resourceArray[16]->number], 48);
+	$vertArray[49] = ($numberMaps[resourceArray[16]->number] + $numberMaps[resourceArray[17]->number], 49);
+	$vertArray[50] = ($numberMaps[resourceArray[17]->number], 50);
+	$vertArray[51] = ($numberMaps[resourceArray[17]->number] + $numberMaps[resourceArray[18]->number], 51);
+	$vertArray[52] = ($numberMaps[resourceArray[18]->number], 52);
+	$vertArray[53] = ($numberMaps[resourceArray[18]->number], 53);
 
-	for($i=0; $i<54; $i++){
-		$var = '$v' + strval(i);
-		$vertArray[i] = eval($var);
-	}
-	sort($vertArray);
+	// for($i=0; $i<54; $i++){
+	// 	$var = '$v' + strval(i);
+	// 	$vertArray[i] = eval($var);
+	// }
+	array_multisort($vertArray[0], SORT_DESC, SORT_NUMERIC, $vertArray[1], SORT_NUMERIC, SORT_DESC);
 
 	for($k=0; $k<11; $k++){
 		echo $vertArray
