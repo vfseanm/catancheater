d68e5b4f0fbb6ec32e145259af1067fe1f5f9db7
Dan Deng <dan.deng@duke.edu>
2013-04-06 21:23:22 -0400

    demo

--- create.php
+++ create.php
@@ -1,14 +1,14 @@
 <?php
-$con=mysqli_connect("example.com","peter","abc123","my_db");
+$con=mysqli_connect("localhost","root","root","catan");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-// Create table
-mysqli_query($con,"INSERT INTO Players (FirstName, ID, Age)
-VALUES ('Peter', 'Griffin',35)");
+$sql="CREATE TABLE players(Name CHAR(50), ID INT, Wood INT, Bricks INT, Sheep INT, Ore INT, Grain INT)";
+
+$sql1 = "CREATE TABLE settlements(ID INT, playerID INT, portID INT, hex1ID INT, hex2ID INT, hex3ID INT)";
 
 // Execute query
 if (mysqli_query($con,$sql))
@@ -19,4 +19,18 @@ else
   {
   echo "Error creating table: " . mysqli_error();
   }
+
+  mysqli_query($con,$sql1);
+
+  mysqli_query($con,"INSERT INTO players (Name, ID, Wood, Bricks, Sheep, Ore, Grain)
+VALUES ('Player1', 1, 0, 0, 0, 0, 0)");
+
+mysqli_query($con,"INSERT INTO players (Name, ID, Wood, Bricks, Sheep, Ore, Grain)
+VALUES ('Player2', 2, 0, 0, 0, 0, 0)");
+
+mysqli_query($con,"INSERT INTO players (Name, ID, Wood, Bricks, Sheep, Ore, Grain)
+VALUES ('Player3', 3, 0, 0, 0, 0, 0)");
+
+mysqli_query($con,"INSERT INTO players (Name, ID, Wood, Bricks, Sheep, Ore, Grain)
+VALUES ('Player4', 4, 0, 0, 0, 0, 0)");
 ?>
